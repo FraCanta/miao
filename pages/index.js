8eec47422956723d3bd57d4dd3cf572197a3b073
FraCanta <you@example.comfcantale14@gmail.com>
2023-08-18 10:40:21 +0200

    hero pages

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import HeroHome from "../components/heros/heroHome";
 import SlideAnimation from "@/components/slideAnimation/slideAnimation";
 import translationIT from "../public/locales/it/it.json";
 import translationEN from "../public/locales/en/en.json";
+import Image from "next/image";
+import SezioneUno from "@/public/sectionsTitle/sezioneUno.png";
+import Link from "next/link";
 
 export default function Home({ translation }) {
   // console.log(translation);
@@ -36,6 +39,50 @@ export default function Home({ translation }) {
       <div className="w-full min-h-[calc(40vh_-_80px)] lg:h-[calc(100vh_-_70px)]  2xl:h-[calc(100vh_-_100px)] fxl:h-[calc(100vh_-_150px)]  4xl:h-[calc(100vh_-_250px)] mx-auto flex flex-col lg:flex-row items-center justify-between">
         <HeroHome translation={translation?.hero} />
       </div>
+      <div className="w-[90%] mx-auto min-h-[40vh] mt-[150px]">
+        <div className="w-full h-full  flex-col justify-start items-start gap-[30px] inline-flex mx-auto">
+          <Image
+            className="object-cover w-[250px] 2xl:w-[300px] fxl:w-[350px] 3xl:w-[400px]"
+            src={SezioneUno}
+            alt="welcome title"
+            width={300}
+            priority
+          />
+          <div className="w-full h-full justify-between flex-col 2xl:flex-row items-center gap-8 2xl:gap-24 inline-flex">
+            <div className="2xl:w-1/2 h-full flex-col justify-start items-start  inline-flex">
+              <p className="text-second text-[1.125rem] fxl:text-[1.4rem] 3xl:text-[1.8rem] font-normal leading-normal">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+            <div className="2xl:w-1/2 h-full flex-col justify-start items-start  inline-flex">
+              <p className="text-second text-[1.125rem] fxl:text-[1.4rem] 3xl:text-[1.8rem] font-normal leading-normal">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          </div>
+          <div className="w-full h-5 justify-start items-center inline-flex mt-6">
+            <Link
+              href="/me"
+              className="text-main text-2xl fxl:text-3xl 3xl:text-4xl font-extrabold underline capitalize leading-tight"
+            >
+              Conosciamoci
+            </Link>
+          </div>
+        </div>
+      </div>
+
       {/* </SlideAnimation> */}
     </>
   );
