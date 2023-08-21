e8322a337e928714de61ff843739afe69cf320d4
FraCanta <you@example.comfcantale14@gmail.com>
2023-08-21 16:22:36 +0200

    logos and title partners

--- next.config.js
+++ next.config.js
@@ -49,7 +49,7 @@ module.exports = withPlugins([
 
   {
     i18n: {
-      locales: ["it", "en"],
+      locales: ["it", "en", "fr"],
       defaultLocale: "it",
       localeDetection: true,
     },
