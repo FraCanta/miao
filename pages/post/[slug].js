ad719b4003159c091328c7eadb26c6f44ba70d2c
FraCanta <you@example.comfcantale14@gmail.com>
2023-09-25 18:18:23 +0100

    slug

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,21 +1,31 @@
 import Image from "next/image";
 import React from "react";
 import { fetchAllPostsSlugs, fetchPost } from "../api/api";
+import Head from "next/head";
 
 const SinglePost = ({ post }) => {
   return (
-    <div className="w-[90%] mx-auto">
-      <h1>{post?.content?.title}</h1>
-      <Image
-        src={post?.content?.image?.filename}
-        alt="img"
-        width={300}
-        height={300}
-        className="w-full"
-      />
+    <>
+      <Head>
+        <title>{post?.name}</title>
+      </Head>
+      <div className="w-[90%] mx-auto ">
+        <h1 className="sm:text-4xl text-3xl mb-4 font-medium py-10 md:py-20">
+          {post?.content?.title}
+        </h1>
+        <Image
+          src={post?.content?.image?.filename}
+          alt="img"
+          width={300}
+          height={300}
+          className="md:h-96 w-full mb-10 object-cover object-center "
+        />
 
-      <p>{post?.content?.content}</p>
-    </div>
+        <p className="mb-8 leading-relaxed text-[1.25rem]">
+          {post?.content?.content}
+        </p>
+      </div>
+    </>
   );
 };
 
