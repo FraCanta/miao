39c60d4239b872bce51579ed792e7c2316fcef3e
FraCanta <you@example.comfcantale14@gmail.com>
2023-10-12 13:52:00 +0100

    sitemap

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,54 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="it">
       <Head>
+        <meta property="og:url" content="https://www.miaographics.it/" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Miao graphics - grafismi itineranti"
+        />
+        <meta
+          property="og:image"
+          content="https://www.miaographics.it/assets/cover_web.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="miaographics.it" />
+        <meta property="twitter:url" content="https://www.miaographics.it/" />
+        <meta
+          name="twitter:title"
+          content="Miao graphics - grafismi itineranti"
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.miaographics.it/assets/cover_web.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />{" "}
+        <meta
+          name="google-site-verification"
+          content="Hqy3nt81_2N8PfqFnJbWpxy6Rhc-rjD1hDTDfUyU2sA"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
