132c1d289e92e7cb00a9bf8838531aef11066e71
FraCanta <you@example.comfcantale14@gmail.com>
2023-11-09 11:04:22 +0000

    pixel

--- pages/_app.js
+++ pages/_app.js
@@ -43,7 +43,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '300681989516435');
+fbq('init', '797931211189612');
 fbq('track', 'PageView');
   `,
         }}
@@ -53,7 +53,7 @@ fbq('track', 'PageView');
           height="1"
           width="1"
           style={{ display: "none" }}
-          src="https://www.facebook.com/tr?id=300681989516435&ev=PageView&noscript=1"
+          src="https://www.facebook.com/tr?id=797931211189612&ev=PageView&noscript=1"
           alt=""
         />
       </noscript>
