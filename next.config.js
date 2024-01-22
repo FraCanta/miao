e9997087e547f645afc5068fff0520f23241feb3
FraCanta <you@example.comfcantale14@gmail.com>
2024-01-22 12:01:16 +0100

    img

--- next.config.js
+++ next.config.js
@@ -68,7 +68,7 @@ module.exports = withPlugins([
   {
     images: {
       formats: ["image/avif", "image/webp"],
-      domains: ["mithablog.mithacreative.it/"],
+      domains: ["mithablog.mithacreative.it/wp-content/uploads/"],
 
       deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
       imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
