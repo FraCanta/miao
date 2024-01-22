56e2835899bbc539c970926c29c3550abaca1ac4
FraCanta <you@example.comfcantale14@gmail.com>
2024-01-22 15:09:36 +0100

    immagini

--- next.config.js
+++ next.config.js
@@ -68,7 +68,10 @@ module.exports = withPlugins([
   {
     images: {
       formats: ["image/avif", "image/webp"],
-      domains: ["mithablog.mithacreative.it/wp-content/uploads/"],
+      domains: [
+        "mithablog.mithacreative.it/wp-content/uploads/",
+        "https://www.miaographics.it/",
+      ],
 
       deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
       imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
