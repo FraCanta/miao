522265e4b8164cf64e224dc8508b743221706cbd
FraCanta <you@example.comfcantale14@gmail.com>
2024-02-08 09:36:13 +0100

    configurazione della mail

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -9,8 +9,8 @@ export default async function mailer(req, res) {
     auth: {
       //   user: process.env.SMTP_USER, se vogliamo inserire le credenziali su env
       //   pass: process.env.SMTP_PASSWORD,
-      user: "thalliondev@gmail.com",
-      pass: "abtryarebjkirtkh",
+      user: "fcantale14@gmail.com",
+      pass: "zhqnyibyfvewrcfe",
     },
   });
 
